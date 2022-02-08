3f91294917b87300eebaa2f7974c801feda385b8
Matthew Fritsch <matthewmfritsch@gmail.com>
2022-02-07 22:11:54 -0800

    Switched project structure to a collection

---
+++ FileIO/target/debug/deps/wordle-f53b7ac8ebece2c0.d
@@ -0,0 +1,5 @@
+/home/matthew/Programming/rust/wordle/target/debug/deps/wordle-f53b7ac8ebece2c0: src/main.rs
+
+/home/matthew/Programming/rust/wordle/target/debug/deps/wordle-f53b7ac8ebece2c0.d: src/main.rs
+
+src/main.rs:
---
+++ FileIO/target/debug/wordle.d
@@ -0,0 +1,1 @@
+/home/matthew/Programming/rust/fileIO/target/debug/wordle: /home/matthew/Programming/rust/fileIO/src/main.rs
