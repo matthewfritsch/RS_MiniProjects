64090d443e7550fc50b1c63e6407eac3cba69d0e
Matthew Fritsch <matthewmfritsch@gmail.com>
2022-02-14 15:50:39 -0800

    Fixed string formatting issue

--- 3. Weatherfetch/src/main.rs
+++ 3. Weatherfetch/src/main.rs
@@ -1,4 +1,5 @@
-use std::{fs, net::ToSocketAddrs};
+// use std::io;
+use std::fs;
 use reqwest::Error;
 
 #[tokio::main]
@@ -6,20 +7,21 @@ async fn main() -> Result<(), Error> {
     let req_prefix = "https://api.weatherapi.com/v1/current.json?key=";
     let api_key = fetch_api_key();
     let req_option = "&q=";
-    let user_loc = "San Jose";
+    let user_loc = "95060";
     let req_suffix = "&aqi=no";
-    let request_url = req_prefix
-    + api_key.to_str() 
-    + req_option 
-    + user_loc
-    + req_suffix;
+    let mut request_url = String::new();
+    request_url.push_str(req_prefix);
+    request_url += &api_key; 
+    request_url.push_str(req_option); 
+    request_url.push_str(user_loc);
+    request_url.push_str(req_suffix);
     let response = reqwest::get(request_url).await?;
 
     println!("{:?}", response.text().await?);
     Ok(())
 }
 
-fn fetch_api_key() -> string {
+fn fetch_api_key() -> String {
     let api_file = "weatherapi_key.txt";
     fs::read_to_string(api_file).expect("File could not be accessed.")
 }
