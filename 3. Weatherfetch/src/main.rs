1c8d5bdf953b8c0b9faccf3c33c13b28d7563adb
Matthew Fritsch <matthewmfritsch@gmail.com>
2022-02-15 22:37:04 -0800

    Added strongly-typed struct definitions for parsing JSON data

--- 3. Weatherfetch/src/main.rs
+++ 3. Weatherfetch/src/main.rs
@@ -2,6 +2,58 @@ use serde_json;
 use serde::{Deserialize, Serialize};
 use std::fs;
 
+//TODO test untyped JSON values to eliminate huge struct definitions
+#[derive(Serialize, Deserialize)]
+struct Condition {
+    text : String,
+    icon : String,
+    code : i16,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Current {
+    last_updated_epoch: i64,
+    last_updated: String,
+    temp_c: f32,
+    temp_f: f32,
+    is_day: i8,
+    condition: Condition,
+    wind_mph: f32,
+    wind_kph: f32,
+    wind_degree: i16,
+    wind_dir: String,
+    pressure_mb: f32,
+    pressure_in: f32,
+    precip_mm: f32,
+    precip_in: f32,
+    humidity: i8,
+    cloud: i8,
+    feelslike_c: f32,
+    feelslike_f: f32,
+    vis_km: f32,
+    vis_miles: f32,
+    uv: f32,
+    gust_mph: f32,
+    gust_kph: f32,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Location {
+    name: String,
+    region: String,
+    country: String,
+    lat: f32,
+    lon: f32,
+    localtime_epoch: i64,
+    localtime: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Weatherdata {
+    location : Location,
+    current : Current,
+}
+
 fn main() {
     //TODO -release-
     // let request_url = _get_request_url();
@@ -43,6 +95,11 @@ fn _fetch_weather_data(req_url : String) -> Result<String, reqwest::Error> {
 fn _fetch_api_key() -> String {
     let api_file = "weatherapi_key.txt";
     fs::read_to_string(api_file).expect("File could not be accessed.")
+}
+
+fn format_json(json : &String) -> Weatherdata {
+    let weather : Weatherdata = serde_json::from_str(json.as_str()).unwrap();
+    weather
 }
 
 /*
